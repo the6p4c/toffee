715668840845638238c05f17b3fe008eb67291f4
the6p4c <me@doggirl.gay>
2023-08-22 14:46:19 +1000

    desktop-entry: parse value types

--- crates/desktop-entry/src/lib.rs
+++ crates/desktop-entry/src/lib.rs
@@ -9,7 +9,7 @@ pub enum Line<'input> {
 }
 
 peg::parser! {
-    grammar desktop_entry_parser() for str {
+    grammar desktop_entry() for str {
         rule whitespace()
             = [' ' | '\t']; // TODO: not all of it lol
 
@@ -39,29 +39,49 @@ peg::parser! {
     }
 }
 
+peg::parser! {
+    grammar value_type() for str {
+        rule ascii_string() -> &'input str
+            = s:$([_]*) { s } // TODO: ascii-only
+        rule utf8_string() -> &'input str
+            = s:$([_]*) { s }
+
+        pub rule string() -> &'input str
+            = ascii_string()
+        pub rule localestring() -> &'input str
+            = utf8_string()
+        pub rule iconstring() -> &'input str
+            = utf8_string()
+        pub rule boolean() -> bool
+            = "true" { true } / "false" { false };
+        pub rule numeric() -> f64
+            = n:$(['+' | '-']?['0'..='9']+("." ['0'..='9']*)?) {? n.parse().or(Err("f64")) }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::desktop_entry_parser as parser;
+    use super::{desktop_entry, value_type};
 
     #[test]
-    fn line_blank() {
+    fn desktop_entry_line_blank() {
         // Any line must end with a linefeed
-        assert!(parser::line_blank("").is_err());
+        assert!(desktop_entry::line_blank("").is_err());
         // A lonely linefeed is a blank line
-        assert_eq!(parser::line_blank("\n"), Ok(()));
+        assert_eq!(desktop_entry::line_blank("\n"), Ok(()));
         // Any whitespace before the linefeed is ignored
-        assert_eq!(parser::line_blank(" \t     \n"), Ok(()));
+        assert_eq!(desktop_entry::line_blank(" \t     \n"), Ok(()));
     }
 
     #[test]
     fn line_comment() {
         // Any line must end with a linefeed
-        assert!(parser::line_comment("").is_err());
+        assert!(desktop_entry::line_comment("").is_err());
         // A comment can be empty
-        assert_eq!(parser::line_comment("#\n"), Ok(""));
+        assert_eq!(desktop_entry::line_comment("#\n"), Ok(""));
         // Comments can contain any character, except for a linefeed
         assert_eq!(
-            parser::line_comment("# this is a comment!!\r\t🥺🥺\n"),
+            desktop_entry::line_comment("# this is a comment!!\r\t🥺🥺\n"),
             Ok(" this is a comment!!\r\t🥺🥺")
         )
     }
@@ -69,23 +89,85 @@ mod tests {
     #[test]
     fn line_group_header() {
         // Any line must end with a linefeed
-        assert!(parser::line_group_header("").is_err());
+        assert!(desktop_entry::line_group_header("").is_err());
         // Group names can be simple strings
-        assert_eq!(parser::line_group_header("[groupname]\n"), Ok("groupname"));
+        assert_eq!(
+            desktop_entry::line_group_header("[groupname]\n"),
+            Ok("groupname")
+        );
         // Group names can also be less simple strings
-        assert_eq!(parser::line_group_header("[group🥺\t name]\n"), Ok("group🥺\t name"))
+        assert_eq!(
+            desktop_entry::line_group_header("[group🥺\t name]\n"),
+            Ok("group🥺\t name")
+        )
     }
 
     #[test]
     fn line_entry() {
         // Any line must end with a linefeed
-        assert!(parser::line_entry("").is_err());
+        assert!(desktop_entry::line_entry("").is_err());
         // Keys must be simple strings, values can be simple strings
-        assert_eq!(parser::line_entry("key=value\n"), Ok(("key", "value")));
+        assert_eq!(
+            desktop_entry::line_entry("key=value\n"),
+            Ok(("key", "value"))
+        );
         // Values can be less simple strings
-        assert_eq!(parser::line_entry("key=val🥺🥺\tue\n"), Ok(("key", "val🥺🥺\tue")));
+        assert_eq!(
+            desktop_entry::line_entry("key=val🥺🥺\tue\n"),
+            Ok(("key", "val🥺🥺\tue"))
+        );
         // Whitespace before and after the = separator is part of the key and value
         // TODO: how is this actually implemented?
-        // assert_eq!(parser::line_entry("key  = value"), Ok(("key  ", " value")));
+        // assert_eq!(desktop_entry::line_entry("key  = value"), Ok(("key  ", " value")));
+    }
+
+    #[test]
+    fn value_type_string() {
+        // ASCII-only data is passed through unchanged
+        assert_eq!(value_type::string("abc\t.\t123"), Ok("abc\t.\t123"));
+        // Non-ASCII is forbidden
+        // TODO: implement this
+        // assert!(value_type::string("abc\t.💜🐶💜\t123").is_err());
+    }
+
+    #[test]
+    fn value_type_localestring() {
+        // Data is passed through unchanged
+        assert_eq!(
+            value_type::localestring("abc\t.💜🐶💜\t123"),
+            Ok("abc\t.💜🐶💜\t123")
+        );
+    }
+
+    #[test]
+    fn value_type_iconstring() {
+        // Data is passed through unchanged
+        assert_eq!(
+            value_type::iconstring("abc\t.💜🐶💜\t123"),
+            Ok("abc\t.💜🐶💜\t123")
+        );
+    }
+
+    #[test]
+    fn value_type_boolean() {
+        // Booleans are either true or false
+        assert_eq!(value_type::boolean("true"), Ok(true));
+        assert_eq!(value_type::boolean("false"), Ok(false));
+        // Anything else is not a boolean
+        assert!(value_type::boolean("blorp").is_err());
+    }
+
+    #[test]
+    fn value_type_numeric() {
+        // Integers are valid
+        assert_eq!(value_type::numeric("1"), Ok(1.0));
+        assert_eq!(value_type::numeric("10"), Ok(10.0));
+        assert_eq!(value_type::numeric("+10"), Ok(10.0));
+        assert_eq!(value_type::numeric("-5"), Ok(-5.0));
+        // Floating point values are valid
+        assert_eq!(value_type::numeric("1.2"), Ok(1.2));
+        assert_eq!(value_type::numeric("10.02"), Ok(10.02));
+        assert_eq!(value_type::numeric("+10.02"), Ok(10.02));
+        assert_eq!(value_type::numeric("-5.5"), Ok(-5.5));
     }
 }
