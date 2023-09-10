f060e3eba743923ad365ed4e5703a6b27836270e
the6p4c <me@doggirl.gay>
2023-09-10 18:39:55 +1000

    desktop-entry: clean up binary a bit

--- crates/desktop-entry/src/main.rs
+++ crates/desktop-entry/src/main.rs
@@ -6,16 +6,17 @@ use desktop_entry::DesktopFile;
 fn main() -> Result<(), String> {
     let args = env::args().collect::<Vec<_>>();
     let filename = match &args[..] {
-        [_, filename] => filename,
-        _ => {
-            return Err(format!("error: expecting the path to a .desktop file as the first and only command-line argument"));
-        }
-    };
+        [_, filename] => Ok(filename),
+        _ => Err(
+            "expected the path to a .desktop file as the first and only command-line argument"
+                .to_string(),
+        ),
+    }?;
 
     let text = fs::read_to_string(filename)
-        .map_err(|err| format!("error: could not read .desktop file - {err}"))?;
+        .map_err(|err| format!("could not read .desktop file - {err}"))?;
     let file = DesktopFile::parse(&text)
-        .map_err(|err| format!("error: could not parse .desktop file - {err}"))?;
+        .map_err(|err| format!("could not parse .desktop file - {err}"))?;
 
     dbg!(file); // TODO: nicer output
 
